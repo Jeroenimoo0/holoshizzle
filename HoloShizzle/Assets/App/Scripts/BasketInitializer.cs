3ef698147f1123867794043f1c2528c4bfbc52b6
Joost van Schaik <joostvanschaik@outlook.com>
2017-11-10 13:39:01 +0100

    Joost's shizzle

---
+++ HoloShizzle/Assets/App/Scripts/BasketInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkitExtensions.Messaging;
+using UnityEngine;
+
+
+public class BasketInitializer : MonoBehaviour
+{
+
+    public GameObject Basket;
+
+	// Use this for initialization
+	void Start ()
+	{
+        Messenger.Instance.AddListener<PositionFoundMessage>(ProcessMessage);
+	    Basket.SetActive(false);
+	}
+
+    private void ProcessMessage(PositionFoundMessage msg)
+    {
+        if (msg.Status == PositionFoundStatus.Accepted)
+        {
+            Basket.SetActive(true);
+            Basket.transform.position = msg.Location;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
